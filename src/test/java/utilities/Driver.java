d09e16b1caa3e65fc8ce9eda74eb67e7feec7150
tugrulcetin <av.tugrulcetin@gmail.com>
2023-12-24 03:28:09 +0100

    Initial commit

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,86 @@
+package utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
+public class Driver {
+
+    public static WebDriver driver;
+    public static int timeout = 5;
+
+    public Driver() {
+        //we don't want to create another abject. Singleton pattern
+    }
+    public static WebDriver getDriver(){
+
+        if (driver == null) {
+
+           // WebDriverManager====== Artik Bonnigarcia olmadigi icin bu class i cagiramiyoruz
+            // Onun yerine seleniumdan hazir driver methodlarini cagiriyoruz
+
+            String browser = ConfigurationReader.getProperty("browser");
+            if("chrome".equals(browser)){
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("ie".equals(browser)) {
+                driver = new InternetExplorerDriver();
+            } else if ("safari".equals(browser)) {
+                driver = new SafariDriver();
+            } else if ("chrome-headless".equals(browser)) {
+
+            }
+
+        }
+
+driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {//if the driver is pointing chrome
+            driver.quit();//quit the driver
+            driver = null;//set it back to null to make sure driver is null
+            // so I can initialize it again
+            //This is important especially you do cross browser testing(testing with
+            // multiple browser like chrome, firefox, ie etc.)
+        }
+    }
+
+    //    Driver.selectByVisibleText(dropdown element, "CHECKING-91303-116.98$")
+    public static void selectByVisibleText(WebElement element, String text) {
+        Select objSelect = new Select(element);
+        objSelect.selectByVisibleText(text);
+    }
+
+    //    Parameter1 : WebElement
+//    Parameter2:  int
+//    Driver.selectByIndex(dropdown element, 1)
+    public static void selectByIndex(WebElement element, int index) {
+        Select objSelect = new Select(element);
+        objSelect.selectByIndex(index);
+    }
+
+    //    Parameter1 : WebElement
+//    Parameter2:  String
+//    Driver.selectByIndex(dropdown element, "91303")
+    public static void selectByValue(WebElement element, String value) {
+        Select objSelect = new Select(element);
+        List<WebElement> elementCount = objSelect.getOptions();
+        objSelect.selectByValue(value);
+        System.out.println("number of elements: " + elementCount.size());
+    }
+
+}
